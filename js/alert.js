6196b227e1ad1d6e65293ba72a1804a6b06c3f35
Jeff Booth <boothsemailaddress@gmail.com>
2016-06-05 12:09:38 -0700

    Alert buttons updated

---
+++ js/alert.js
@@ -0,0 +1,6 @@
+$("#alert-button").click(function() {
+    $("#contact-info").hide();
+    $("#alert-info").show();
+    $("#tracker").removeClass("alert-success");
+    $("#tracker").addClass("alert-danger");
+});
